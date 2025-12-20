67429ecff6c0b3d464bd8891f2a89a09cc4844c5
Florian Warin <florian.warin05@gmail.com>
2025-12-20 22:11:19 +0100

    Test changement index.js

--- chronological_clicker-main/era_clicker_v02-12/index.js
+++ chronological_clicker-main/era_clicker_v02-12/index.js
@@ -1,38 +1,20 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose'); 
 const cors = require('cors');
 
-// 1. Configuration de base (Sécurité et format des données)
-app.use(cors()); 
-app.use(express.json()); // Indispensable pour recevoir des scores plus tard
-app.use(express.urlencoded({ extended: true }));
+const app = express();
 
-// 2. LE PLUS IMPORTANT : Servir votre jeu
-// Cela dit : "Quand on va sur le site, affiche le contenu du dossier 'public'"
-// C'est grâce à cette ligne que votre index.html va s'afficher.
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+mongoose.connect('mongodb+srv://Flowarin:sagisagi@clustertesthtml.v5zecsu.mongodb.net/?appName=ClusterTestHTML')
+.then(() => console.log("Connection to mongoDB established"))
+.catch((err) => console.log("Connection to mongoDB failed"))
 
-// 3. VOS FUTURES ROUTES (API)
-// Pour l'instant, c'est vide, mais c'est ici que vous ajouterez la logique
-// pour sauvegarder. Je vous mets un exemple prêt pour plus tard.
-
-// Exemple : Route pour sauvegarder (À utiliser plus tard avec fetch)
-app.post('/api/save', (req, res) => {
-    const donneesRecues = req.body;
-    console.log("Sauvegarde reçue du jeu :", donneesRecues);
-    
-    // Ici, plus tard, on enregistrera dans une base de données
-    
-    res.json({ status: "success", message: "Bien reçu chef !" });
-});
-
-
-// 4. Démarrage du serveur
 app.listen(8080, () => {
-    console.log("------------------------------------------------");
-    console.log("LE SERVEUR EST PRÊT !");
-    console.log("Pour jouer, ouvrez votre navigateur ici :");
-    console.log("http://localhost:8080");
-    console.log("------------------------------------------------");
+
+    console.log("Server started on port : 8080");
+    
 });
